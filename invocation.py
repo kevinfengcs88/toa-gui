c207da31e82dfbcbbf87582014d80c914d1cf5d4
Kevin Feng <kevinfeng.cs88@gmail.com>
2023-09-03 17:15:23 -0400

    Button now updates raid level label

--- invocation.py
+++ invocation.py
@@ -1,8 +1,8 @@
 class Invocation:
-    def __init__(self, name, description, raidlevel, category):
+    def __init__(self, name, description, points, category):
         self._name = name
         self._description = description
-        self._raidlevel = raidlevel
+        self._points = points
         self._category = category
 
     def get_name(self):
@@ -11,8 +11,8 @@ class Invocation:
     def get_description(self):
         return self._description
 
-    def get_raidlevel(self):
-        return self._raidlevel
+    def get_points(self):
+        return self._points
 
     def get_category(self):
         return self._category
@@ -23,11 +23,11 @@ class Invocation:
     def set_description(self, description):
         self._description = description
 
-    def set_raidlevel(self, raidlevel):
-        self._raidlevel = raidlevel
+    def set_points(self, points):
+        self._points = points
 
     def set_category(self, category):
         self._category = category
 
     def __str__(self):
-        return f"Invocation(name='{self._name}', description='{self._description}', raidlevel={self._raidlevel}, category='{self._category}')"
+        return f"Invocation(name='{self._name}', description='{self._description}', points={self._points}, category='{self._category}')"
--- main.py
+++ main.py
@@ -29,17 +29,24 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure((0, 1), weight=1)
         self.grid_rowconfigure(0, weight=1)
 
+        self.raid_level = 0
+
         self.invocation_frame = InvocationsFrame(self, all_invocations.all_invocations.values())
         self.invocation_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
 
-        self.raidlevel = customtkinter.CTkLabel(self, text="0", fg_color="transparent", font=("Sans Serif", 20))
-        self.raidlevel.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="nsew")
+        self.raid_level_label = customtkinter.CTkLabel(self, text="0", fg_color="transparent", font=("Sans Serif", 20))
+        self.raid_level_label.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="nsew")
 
-        self.button = customtkinter.CTkButton(self, text="Print Active Invocations", command=self.button_callback)
+        self.button = customtkinter.CTkButton(self, text="Update Raid Level", command=self.button_callback)
         self.button.grid(row=1, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
 
     def button_callback(self):
-        print("Active invocations are:", self.invocation_frame.get())
+        active_invocations = self.invocation_frame.get()
+        print("Active invocations are:", active_invocations)
+        self.raid_level = 0
+        for invocation in active_invocations:
+            self.raid_level += all_invocations.all_invocations[invocation].get_points()
+        self.raid_level_label.configure(text=str(self.raid_level))
 
 app = App()
 app.mainloop()
