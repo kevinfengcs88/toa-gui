4354a627f3af61aff11cc9164d5e8296c15a2ba0
Kevin Feng <kevinfeng.cs88@gmail.com>
2023-09-04 13:17:23 -0400

    Progress bar with proper colors updates alongside raid level label

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ class InvocationsFrame(customtkinter.CTkScrollableFrame):
         return active_invocations
 
 
+class RaidLevelFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -27,15 +32,24 @@ class App(customtkinter.CTk):
         self.title("my app")
         self.geometry("800x600")
         self.grid_columnconfigure((0, 1), weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure((0), weight=1)
 
         self.raid_level = 0
 
         self.invocation_frame = InvocationsFrame(self, all_invocations.all_invocations.values())
         self.invocation_frame.grid(row=0, column=0, padx=10, pady=(10, 10), sticky="nsew")
 
-        self.raid_level_label = customtkinter.CTkLabel(self, text="0", fg_color="transparent", text_color="yellow", font=("Sans Serif", 20))
-        self.raid_level_label.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="nsew")
+        self.raid_level_frame = RaidLevelFrame(self)
+        self.raid_level_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+
+        self.raid_level_frame.grid_columnconfigure(0, weight=1)
+
+        self.raid_level_label = customtkinter.CTkLabel(self.raid_level_frame, text="0", fg_color="transparent", text_color="yellow", font=("Sans Serif", 20))
+        self.raid_level_label.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+
+        self.raid_level_progress_bar = customtkinter.CTkProgressBar(self.raid_level_frame, orientation="horizontal", border_color="black", border_width=1, progress_color="yellow")
+        self.raid_level_progress_bar.set(0)
+        self.raid_level_progress_bar.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="ew")
 
 
     def update_raid_level(self):
@@ -49,10 +63,16 @@ class App(customtkinter.CTk):
         self.raid_level_label.configure(text=str(self.raid_level))
         if self.raid_level < 150:
             self.raid_level_label.configure(text_color="yellow")
+            self.raid_level_progress_bar.configure(progress_color="yellow")
         elif self.raid_level >= 150 and self.raid_level < 300:
             self.raid_level_label.configure(text_color="blue")
+            self.raid_level_progress_bar.configure(progress_color="blue")
         elif self.raid_level >= 300:
             self.raid_level_label.configure(text_color="red")
+            self.raid_level_progress_bar.configure(progress_color="red")
+
+        # update the progress bar
+        self.raid_level_progress_bar.set(self.raid_level/600)
 
 
 app = App()
